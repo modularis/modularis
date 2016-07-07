7a4b5cee139287a7e58eff21a76e50c4b6ab2a3f
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-07-07 20:47:22 +0200

    add mocha test example file, fixes #24

---
+++ test/test.js
@@ -0,0 +1,13 @@
+describe('modularis', () => {
+  const expect = require('chai').expect;
+
+  it('should be TESTCASE', () => {
+
+  });
+
+  describe('#NESTED()', () => {
+    it('should be NESTED TESTCASE', () => {
+
+    });
+  });
+});
