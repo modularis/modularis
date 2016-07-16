88d56e33646f8dcfdaad6851574e1cdfa374c2ce
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-07-16 19:48:36 +0200

    add browser testing with zombie.js. fixes #27

---
+++ test/browser.js
@@ -0,0 +1,18 @@
+const Browser = require('zombie');
+Browser.localhost('example.com', 3000);
+
+describe('User visits homepage', () => {
+  const browser = new Browser();
+
+  before((done) => {
+    browser.visit('', done);
+  });
+ 
+  it('should be successful', () => {
+    browser.assert.success();
+  });
+
+  it('should see homepage', () => {
+    browser.assert.text('title', 'modularis');
+  });
+});
