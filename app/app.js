43e7bba1ce62d154c99e4f1d26f55bb74037d616
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-08-08 20:29:35 +0200

    add experimental express code

---
+++ app/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const TemplateLoader = require('./lib/TemplateLoader.js');
+
+const app = express();
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/template/:template', (req, res) => {
+  const templateLoader = new TemplateLoader(req, res);
+  templateLoader.deliver();
+});
+
+app.listen(5000);
--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
+/* eslint-env browser */
 // Example usage:
 // import leftPad from 'left-pad';
 // let foo = leftPad('bar', 2);
+
+const request = new Request('/template/button');
+
+fetch(request)
+  .then((response) => {
+    response.blob().then(res => {
+      console.log(res);
+    });
+  }).catch((error) => {
+    console.log(error);
+  });
