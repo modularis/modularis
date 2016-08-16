b7e694cab48132ac731b5c7b0a312dce2deda3ff
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-08-16 20:01:17 +0200

    fix case sensitive renaming fuck up

---
+++ resources/js/app/view.js
@@ -0,0 +1,69 @@
+/* eslint-env browser */
+import Handlebars from 'handlebars/runtime';
+
+export default class View {
+  constructor($el) {
+    this.el = $el;
+    this.viewStore = {};
+  }
+
+  store(view, path) {
+    this.viewStore[path] = view;
+  }
+
+  loadTemplate(templatePath) {
+    return new Promise((promiseResolve) => {
+      const request = new Request(`/view-loader/${templatePath}`);
+      fetch(request)
+        .then((response) => {
+          response.text().then(template => {
+            const compile = new Function('return ' + template);
+            promiseResolve(Handlebars.template(compile()));
+          });
+        }).catch((error) => {
+          console.log(error);
+        });
+    });
+  }
+
+  loadData(path) {
+    return new Promise((promiseResolve) => {
+      const headers = new Headers();
+      headers.append('Accept', 'json');
+      const request = new Request(path);
+      fetch(request, { headers })
+        .then((response) => {
+          response.text().then(json => promiseResolve(json));
+        }).catch((error) => {
+          console.log(error);
+        });
+    });
+  }
+
+  extractData() {
+    return new Promise((promiseResolve) => {
+      // @TODO: get real data.
+      const data = {};
+      if (this.el.dataset.cName) {
+        data[this.el.dataset.cName] = this.el.dataset.cValue || this.el.innerHTML;
+      }
+      // console.log(this.el.dataset.cName);
+      // console.log(this.el.querySelectorAll('[data-c-name]'));
+      promiseResolve(data);
+    });
+  }
+
+  static replaceElement($el, markup) {
+    const $elWrap = document.createElement('div');
+    $elWrap.innerHTML = markup;
+
+    const $newEl = $elWrap.firstChild;
+    const $parentNode = $el.parentNode;
+
+    if (!$parentNode) return false;
+
+    $parentNode.replaceChild($newEl, $el);
+
+    return $newEl;
+  }
+}
---
+++ resources/js/controller/index.js
@@ -0,0 +1,15 @@
+import app from '../app/app.js';
+import Controller from '../app/controller.js';
+
+import Button from '../../components/button/index.js';
+import Card from '../../components/card/index.js';
+
+class Index extends Controller {
+  constructor() {
+    super();
+    const button = new Button(document.querySelector('.c-button'), 'components/button/template');
+    const card = new Card(document.querySelector('.c-card'), 'components/card/template');
+  }
+}
+
+app.boot(Index);
