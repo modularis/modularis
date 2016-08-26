1470996f3d54fe839dba14cb7dd40314446a8801
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-08-26 17:04:11 +0200

    catch errors in the bundle build process

--- build/scripts-bundle.js
+++ build/scripts-bundle.js
@@ -24,7 +24,7 @@ controllers.forEach((file) => {
       dest: `app/public/js/${path.parse(file).base}`,
       sourceMap: 'inline'
     });
-  });
+  }).catch((error) => console.log(error)); // eslint-disable-line no-console
 });
 
 const serviceWorker = 'resources/js/app/service-worker.js';
@@ -32,5 +32,5 @@ rollup.rollup(Object.assign({ entry: serviceWorker }, config)).then((bundle) => 
   bundle.write({
     format: 'iife',
     dest: `app/public/${path.parse(serviceWorker).base}`
-  });
+  }).catch((error) => console.log(error)); // eslint-disable-line no-console
 });
