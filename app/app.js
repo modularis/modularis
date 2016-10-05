e06b362993a9f50383cd0e6f45240d5f58d1b1c0
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-10-05 12:04:08 +0200

    Add proof of concept implementation of cache busting mechanism

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 const compression = require(`compression`);
 const express = require(`express`);
 const exphbs = require(`express-handlebars`);
+const glob = require(`glob`);
 const path = require(`path`);
 
 const IndexController = require(`./controller/IndexController.js`);
@@ -20,6 +21,31 @@ app.engine(`.hbs`, exphbs({
 
 app.set(`view engine`, `.hbs`);
 app.set(`views`, path.join(__dirname, `../resources/views`));
+
+
+let css = glob.sync(path.join(__dirname, `public`, `**`, `*.css`));
+css = css.reduce((previous, x) => {
+  const relPath = path.relative(path.join(__dirname, `public`), x);
+  const pathObject = path.parse(relPath);
+  previous[pathObject.name] = path.join(`/`, relPath);
+  return previous;
+}, {});
+
+let js = glob.sync(path.join(__dirname, `public`, `**`, `*.js`));
+js = js.reduce((previous, x) => {
+  const relPath = path.relative(path.join(__dirname, `public`), x);
+  const pathObject = path.parse(relPath);
+  previous[pathObject.name] = path.join(`/`, relPath);
+  return previous;
+}, {});
+
+app.use((req, res, next) => {
+  res.locals = {
+    css,
+    js
+  };
+  next();
+});
 
 app.get(`/`, (request, response) => {
   const indexController = new IndexController(request, response);
